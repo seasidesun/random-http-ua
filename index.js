00b4cf167c9d1531b00ccb3f2c553b2d3de0760a
haibinsun <seasidetank@gmail.com>
2019-03-06 23:22:36 +0800

    feat: add the pc os info

--- index.js
+++ index.js
@@ -1,15 +1,59 @@
 (function(){
+
+// randomMachine
+const rM = (list) => {
+    if (!(list instanceof Array)) return list
+
+    let seed = parseInt(Math.random().toString().slice(-3))
+    let index = seed % list.length
+
+    return list[index]
+}
+
+let rD = {
+    'windowVer': ['5.1', '6.1', '10.0'],
+    'windowBit': ['WOW64', 'Win64; x64'],
+    'macosVer': ['10_13_6', '10_14_3'],
+}
+
+let osHandler = {
+    'window': () => {
+        return `Windows NT ${rM(rD.windowVer)}; ${rM(rD.windowBit)}`
+    },
+    'macos': () => {
+        return `Macintosh; Intel Mac OS X ${rM(rD.macosVer)}`
+    },
+}
+
+let createOsinfo = (opts) => {
+    let ret
+    switch (opts.device) {
+        case 'pc':
+            ret = osHandler[opts.os]
+            break;
+        case 'mobile':
+            break;
+        default:
+            break;
+    }
+    return ret
+}
+
+
 /**
  * 生成userAgent
  * @param {Object} opts
  * device: pc | mobile
  * os: windows | macos | ios | android
  */
-let createOneUa = () => {
+let createOneUa = (opts) => {
     // 基金会
     let foundation = 'Mozilla/5.0'
 
-    return foundation
+    // 系统相关
+    let osInfo = createOsinfo(opts)
+
+    return foundation + osInfo
 }
 
 let createUas = (num = 1, opts) => {
