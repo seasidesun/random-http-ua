517a7e4214b3e2b27a401bb319406df813ec55f8
haibinsun <haibinsun@tencent.com>
2019-03-07 19:49:00 +0800

    feat: add engine version

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 (function(){
 
 // randomMachine
-const rM = (list) => {
+const rML = (list) => {
     if (!(list instanceof Array)) return list
 
     let seed = parseInt(Math.random().toString().slice(-3))
@@ -10,10 +10,39 @@ const rM = (list) => {
     return list[index]
 }
 
+const rMR = (from, to) => {
+    from = parseInt(from)
+    to = parseInt(to)
+
+    if (isNaN(from)) return ''
+    if (isNaN(to)) to = 0
+    if (from === to) return from
+
+    const bottom = from < to ? from : to
+    const range = Math.abs(from - to) + 1
+    let seed = parseInt(Math.random().toString().slice(-4))
+    let index = seed % range
+
+    return bottom + index
+}
+
+const rMP = (rate) => {
+    let seed = parseInt(Math.random().toString().slice(2, 3))
+    return seed < rate
+}
+
+const rMPR = (rate, value) => {
+    return rMP(rate) ? value : ''
+}
+
 let rD = {
     'windowVer': ['5.1', '6.1', '10.0'],
     'windowBit': ['WOW64', 'Win64; x64'],
-    'macosVer': ['10_13_6', '10_14_3'],
+    'macosVer': ['10_13_1', '10_13_2', '10_13_3', '10_13_4', '10_13_5', '10_13_6', '10_14_1', '10_14_2', '10_14_3'],
+    'engineVer': () => `${rMR(412, 605)}.${rMR(1, 10)}${rMPR(8, `.${rMR(1, 60)}`)}`,
+    'versionVer': () => `${rMR(4, 12)}.${rMR(0, 5)}`,
+    'chromVer': () => `${rMR(50, 73)}.0.${rMR(2500, 3500)}.${rMR(1, 100)}`,
+    'safariVer': () => `${rMR(537, 605)}.${rMR(1, 36)}${rMPR(8, `.${rMR(1, 20)}`)}`,
 }
 
 let osHandler = {
@@ -53,7 +82,10 @@ let createOneUa = (opts) => {
     // 系统相关
     let osInfo = createOsinfo(opts)
 
-    return foundation + osInfo
+    // engine
+    let engine = `AppleWebKit/${rD.engineVer()} (KHTML\, like Gecko)`
+
+    return foundation + osInfo + engine
 }
 
 let createUas = (num = 1, opts) => {
