6a00bb2663d87af04fde7d1e5025634b27920d83
haibinsun <haibinsun@tencent.com>
2019-03-13 16:18:50 +0800

    style：up the comments

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 /**
  * 根据所给概率返回此次随机是否落在概率区间
  * @param {Number} rate 概率 0-10
+ * @return {Boolean} 是否概率落在区间
  */
 const rMP = (rate) => {
     let seed = parseInt(Math.random().toString().slice(2, 4))
@@ -11,6 +12,7 @@ const rMP = (rate) => {
 /**
  * 根据所给概率返回此次随机是否有值
  * @param {String} data 概率^有效值
+ * @return {String} 有效值
  */
 const rMPR = (data) => {
     data = data.split('^')
@@ -21,6 +23,7 @@ const rMPR = (data) => {
 /**
  * 随机输出数组内的元素
  * @param {Array || String} list 供随机的数据
+ * @return {String} 随机出的数据
  */
 const rML = (list) => {
     if (!(list instanceof Array)) list = [list]
@@ -55,6 +58,7 @@ const rML = (list) => {
 /**
  * 随机26字母
  * @param {Number} num 字母的长度
+ * @return {String} 随机出的字母
  */
 const rMW = (num = 1) => {
     let ret = ''
@@ -68,6 +72,7 @@ const rMW = (num = 1) => {
  * 随机输出区间内的一个数字
  * @param {Number || String} from 最小值
  * @param {Number || String} to 最大值
+ * @return {String} 随机出的数字
  */
 const rMR = (from, to) => {
     from = parseInt(from)
@@ -86,22 +91,56 @@ const rMR = (from, to) => {
 }
 
 /**
- * 将数组里的元素拼成一个字符串，'' | ';'过滤
+ * 将数组里的元素拼成一个字符串，['', ';']过滤掉
  * @param {Array || String} list
+ * @return {String} ua的一部分
  */
-const concatlList = (list) => {
+const cL = (list) => {
     if (!(list instanceof Array)) list = [list]
     return list.filter((item) => {return !(['', ';'].includes(item))}).join(' ')
 }
 
 /**
  * 供随机的数据
+ * device: 设备类型 mobile || pc
+ * 
+ * pcOs: pc端操作系统
+ * mobileOs: 移动端操作系统
+ * 
+ * windowsVer: windows系统版本
+ * windowsBit: windows系统Bit位
+ * windowsApp: windows系统下的浏览器应用 eg.ie、chrome
+ * 
+ * macosVer: macos系统版本
+ * macosApp: macos系统下的浏览器应用 eg.safari、chrome
+ * 
+ * iosVer: ios版本
+ * iosApp: ios浏览器应用
+ * 
+ * androidVer: 安卓版本
+ * androidDevice: 安卓机型 eg.samsung、huawei、mi
+ * androidApp: 安卓浏览器应用 eg.uc、wechat
+ * 
+ * UA中的部分字符串随机：
+ * engineVer: ua中浏览引擎版本
+ * versionVer: ua中浏览引擎版本加号版本
+ * chromeVer: ua中chrome版本
+ * safariVer: ua中safari版本
+ * mobileVer: ua中Mobile版本
+ * networwVer: ua中网络类型
+ * netVer: ua中'.net'版本，重要是ie中使用到
+ * ieVer: ie版本
+ * tridentVer: trident版本，主要ie中使用到
+ * qbVer: qq浏览器版本
+ * qbcoreVer: qq浏览器core版本
+ * edgeVer: egde浏览器版本
+ * firefoxVer: firefox浏览器版本
  */
 const rD = {
-    'device': () => rML(['mobile']), // pc
+    'device': () => rML(['mobile', 'pc']),
 
     'pcOs': () => rML(['windows', 'macos']),
-    'mobileOs': () => rML(['android']), // ios
+    'mobileOs': () => rML(['android', 'ios']),
 
     'windowsVer': () => rML(['5.1', '6.1', '10.0']),
     'windowsBit': () => rML(['WOW64', 'Win64; x64']),
@@ -134,14 +173,14 @@ const rD = {
 }
 
 /**
- * ua系统信息
+ * ua-系统信息部分，一般放在ua的引擎后面
  */
 const osHandlerOfpc = {
     'windows': () => {
         return `Windows NT ${rD.windowsVer()}; ${rD.windowsBit()}`
     },
     'windows-ie': () => {
-        return concatlList([
+        return cL([
             `compatible; MSIE ${rD.ieVer()};`,
             `Windows NT ${rD.windowsVer()};`, 
             `${rMPR(`50^${rD.windowsBit()}`)};`,
@@ -168,7 +207,8 @@ const osHandlerOfpc = {
         return `Linux; Android ${rD.androidVer()}; ${tag} Build/HUAWEI${tag}; wv`
     },
     'android-mi': () => {
-        let miVerList = ['4LTE', '5', '5X', '5C', '5s', '5s Plus', '6', '6X', '8', '8 UD', '8 SE', '8 Lite', '9', 'MAX', 'MAX 2', 'MAX 3', 'PAD', 'PAD 4', 'NOTE', 'NOTE LTE']
+        let miVerList = ['4LTE', '5', '5X', '5C', '5s', '5s Plus', '6', '6X', '8', '8 UD',
+                        '8 SE', '8 Lite', '9', 'MAX', 'MAX 2', 'MAX 3', 'PAD', 'PAD 4', 'NOTE', 'NOTE LTE']
         let miVer = rML([`MI ${rML(miVerList)}`, `MIX ${rML(['2', '2s', '3'])}`])
         let suf1 = `Build/${rML(['OPM1', 'PKQ1', 'OPR1'])}.${rMR(170000, 180729)}.${rMR(170000, 180729)}`
         let suf2 = `Build/${rMW(3)}${rMR(10, 30)}${rMW(1)}`
@@ -176,24 +216,39 @@ const osHandlerOfpc = {
         return `Linux; Android ${rD.androidVer()}; ${miVer} ${suf}; wv`
     },
     'android-oppo': () => {
-        let oppoVerList = ['r9 plustm a', 'A57t', 'R9m', 'R9t', 'A79t', 'R11t', 'A57', 'A37m', 'R11', 'A59s', 'R9sk', 'A73t', 'A83', 'R9 Plustm A', 'R9s', 'R9tm', 'R11s', 'A77', 'A79k', 'R11st', 'A77t', 'A33', 'A53', 'A59m', 'R11 Plus', 'R9s Plus', 'A83t', 'R9', 'A59t', 'R9skt', 'A37t', 'A53m', 'A73', 'R7sm', 'R7s', 'R9st', 'r7s', 'r9', 'a37m', 'r9m', 'R11 Plusk', 'r9 plusm a']
+        let oppoVerList = ['r9 plustm a', 'A57t', 'R9m', 'R9t', 'A79t', 'R11t', 'A57', 'A37m',
+                        'R11', 'A59s', 'R9sk', 'A73t', 'A83', 'R9 Plustm A', 'R9s', 'R9tm', 'R11s',
+                        'A77', 'A79k', 'R11st', 'A77t', 'A33', 'A53', 'A59m', 'R11 Plus', 'R9s Plus',
+                        'A83t', 'R9', 'A59t', 'R9skt', 'A37t', 'A53m', 'A73', 'R7sm', 'R7s', 'R9st',
+                        'r7s', 'r9', 'a37m', 'r9m', 'R11 Plusk', 'r9 plusm a']
         let tag = `OPPO ${rML(oppoVerList)} Build/${rMW(3)}${rMR(10, 30)}${rMW(1)}`
         return `Linux; Android ${rD.androidVer()}; ${tag}; wv`
     },
     'android-vivo': () => {
-        let vivoVerList = ['X21i A', 'X9Plus', 'Y66L', 'Y66', 'X20', 'V3Max A', 'X7', 'NEX A', 'Y31', 'X9i', 'Y85A', 'Y75A', 'X9s', 'Xplay6', 'Y83A', 'X7Plus', 'X9s Plus', 'X9s L', 'X9', 'X9L', 'X6Plus D', 'Y71', 'X21UD A', 'Y51A', 'X6Plus L', 'Y71A', 'X20A', 'Y66i A', 'X6A', 'X21', 'X9Plus L', 'Y53L', 'V3M A', 'X6D', 'X21A', 'V3', 'Y66i', 'X6S A', 'Z1', 'X3V', 'Y67L', 'X20Plus A', 'Xplay5A', 'Z1i', 'NEX S']
+        let vivoVerList = ['X21i A', 'X9Plus', 'Y66L', 'Y66', 'X20', 'V3Max A', 'X7', 'NEX A', 'Y31',
+                        'X9i', 'Y85A', 'Y75A', 'X9s', 'Xplay6', 'Y83A', 'X7Plus', 'X9s Plus', 'X9s L',
+                        'X9', 'X9L', 'X6Plus D', 'Y71', 'X21UD A', 'Y51A', 'X6Plus L', 'Y71A', 'X20A',
+                        'Y66i A', 'X6A', 'X21', 'X9Plus L', 'Y53L', 'V3M A', 'X6D', 'X21A', 'V3', 'Y66i', 
+                        'X6S A', 'Z1', 'X3V', 'Y67L', 'X20Plus A', 'Xplay5A', 'Z1i', 'NEX S']
         let tag = `vivo ${rML(vivoVerList)} Build/${rML(['OPM1', 'PKQ1', 'OPR1'])}.${rMR(17000, 180128)}.0${rMR(1, 99)}`
         return `Linux; Android ${rD.androidVer()}; ${tag}; wv`
     },
     'android-samsung': () => {
-        let samsungVerList = ['G9550', 'C7000', 'G9500', 'C5000', 'G9600', 'N9500', 'G9200', 'G9508', 'G6100', 'G9287', 'N9600', 'C7010', 'G9350', 'G9280', 'N9006', 'G9300', 'J3109', 'C9000', 'G935F', 'N950N', 'G9650', 'A500FU', 'C5010', 'J5008', 'A5000', 'G955F', 'A9100', 'E7009', 'G8508S', 'W2018', 'J3110', 'G955U', 'G5700', 'A7100', 'A7000', 'G9608', 'G8850', 'G8750', 'A600N', 'A8000', 'J7008', 'G950U', 'N9508', 'G5108Q', 'N9108V', 'G9008V', 'N920I', 'G610F', 'A6050', 'W2016', 'C9008', 'G9750', 'G3606', 'A5108', 'J5108', 'P585Y', 'G928V', 'T331C', 'N9005', 'N960F', 'G7105', 'N9200', 'G1600', 'J250F', 'N9008V', 'G8870']
+        let samsungVerList = ['G9550', 'C7000', 'G9500', 'C5000', 'G9600', 'N9500', 'G9200', 'G9508',
+                        'G6100', 'G9287', 'N9600', 'C7010', 'G9350', 'G9280', 'N9006', 'G9300', 'J3109',
+                        'C9000', 'G935F', 'N950N', 'G9650', 'A500FU', 'C5010', 'J5008', 'A5000', 'G955F',
+                        'A9100', 'E7009', 'G8508S', 'W2018', 'J3110', 'G955U', 'G5700', 'A7100', 'A7000',
+                        'G9608', 'G8850', 'G8750', 'A600N', 'A8000', 'J7008', 'G950U', 'N9508', 'G5108Q',
+                        'N9108V', 'G9008V', 'N920I', 'G610F', 'A6050', 'W2016', 'C9008', 'G9750', 'G3606',
+                        'A5108', 'J5108', 'P585Y', 'G928V', 'T331C', 'N9005', 'N960F', 'G7105', 'N9200',
+                        'G1600', 'J250F', 'N9008V', 'G8870']
         let tag = `SM-${rML(samsungVerList)} Build/${rMW(`${rMR(1, 3)}`)}${rMR(0, 99)}${rMW(1)}`
         return `Linux; Android ${rD.androidVer()}; ${tag}; wv`
     },
 }
 
 /**
- * ua应用信息 eg. ie safari 360 chrome
+ * ua-应用信息部分，一般放在ua的最后面 eg. ie safari 360 chrome
  */
 const appSufHandlerOfpc = {
     'windows': () => {
@@ -246,13 +301,14 @@ const appSufHandlerOfpc = {
 /**
  * 生成一个pc user-agent
  * @param {Object} opts
+ * @return {String} ua
  */
 const genOneUaOfpc = (opts) => {
     // 基金
     let foundation = 'Mozilla/5.0'
 
     // 系统
-    let tag = `${opts.os}-${opts.app}`
+    let tag = `${opts.os}-${opts.app}` // eg. windows-ie、macos-chrome
     let osHandler = osHandlerOfpc[`${tag}`] || osHandlerOfpc[`${opts.os}`]
     let osInfo = osHandler()
 
@@ -264,7 +320,7 @@ const genOneUaOfpc = (opts) => {
     let chrome = `Chrome/${rD.chromeVer()}`
     let safari = `Safari/${rD.safariVer()}`
     let prefix = rML([`50^${version}`, `${chrome}`])
-    let suffix = concatlList([`${prefix}`, `${safari}`])
+    let suffix = cL([`${prefix}`, `${safari}`])
 
     let ua = ''
     switch (tag) {
@@ -288,19 +344,20 @@ const genOneUaOfpc = (opts) => {
 /**
  * 生成一个mobile user-agent
  * @param {Object} opts
+ * @return {String} ua
  */
-let genOneUaOfmobile = (opts) => {
+const genOneUaOfmobile = (opts) => {
     // 基金
     let foundation = 'Mozilla/5.0'
 
     // 系统
     let osInfo = ''
     if (opts.os === 'ios') {
-        let tag = `${opts.os}-${opts.app}`
+        let tag = `${opts.os}-${opts.app}` // eg. os-wechat、ios-qb
         let osHandler = osHandlerOfpc[`${tag}`] || osHandlerOfpc[`${opts.os}`]
         osInfo = osHandler()
     } else {
-        let deviceTag = `${opts.os}-${opts.brand}`
+        let deviceTag = `${opts.os}-${opts.brand}` // eg. android-huawei、android-mi
         let osHandler = osHandlerOfpc[`${deviceTag}`] || osHandlerOfpc[`${opts.os}`]
         osInfo = osHandler()
     }
@@ -320,7 +377,7 @@ let genOneUaOfmobile = (opts) => {
     }
 
     let ua = ''
-    let tag = `${opts.os}-${opts.app}`
+    let tag = `${opts.os}-${opts.app}` // eg. ios-wechat、android-uc
     let appSuf = appSufHandlerOfpc[`${tag}`] || appSufHandlerOfpc[`${opts.os}`]
     switch (tag) {
         case 'ios-':
@@ -334,10 +391,11 @@ let genOneUaOfmobile = (opts) => {
 }
 
 /**
- * 生成一个user-agent
- * @param {Object} opts
+ * 生成一个ua
+ * @param {Object} opts 浏览器系统环境
+ * @return {String} ua
  */
-let genOneUa = (opts) => {
+const genOneUa = (opts) => {
     switch (opts.device) {
         case 'pc':
             return genOneUaOfpc(opts)
@@ -350,16 +408,17 @@ let genOneUa = (opts) => {
 
 /**
  * 随机生成一个浏览器的系统环境
+ * @return {Object} 浏览器系统环境
  * device: pc | mobile
  * os: windows | macos | andriod | ios
  * app: safari | wechat | chrome | qb
  * brand: huawei | mi | oppo | vivo | samsung
  */
-let genUaType = () => {
+const genUaType = () => {
     const device = rD.device()
     const os = rD[`${device}Os`]()
     const app = rD[`${os}App`]()
-    const brand = `${device}${os}` === 'mobileandroid' ? rD.androidDevice() : ''
+    const brand = `${device}${os}` === 'mobileandroid' ? rD.androidDevice() : '' // 只有安卓机器分机型
 
     return {
         device,
@@ -369,7 +428,12 @@ let genUaType = () => {
     }
 }
 
-let generateUa = (num = 1) => {
+/**
+ * 随机生成浏览器的ua
+ * @param {String | Number} ua的数量
+ * @return {String | Array} 浏览器ua
+ */
+const generateUa = (num = 1) => {
     num = parseInt(num)
     if (isNaN(num) || num < 1) num = 1
 
