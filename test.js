c3175f4b1f74d9e6a4d277afa30b7c092d1c51a1
haibinsun <haibinsun@tencent.com>
2019-03-19 13:00:37 +0800

    build: v1.0.2

--- test.js
+++ test.js
@@ -1,26 +1,39 @@
-let uaO = require('./index.js')
 let uaT = require('./random-http-ua.js')
-let uaM = require('./random-http-ua.min.js')
-
-let result1 = uaO.generateUa(20, {
-    device: ['pc'],
-    pcOs: ['macos', '90^windows'],
-})
-let result2 = uaT.generateUa(20, {
-    device: ['mobile'],
-    mobileOs: ['android', '90^ios'],
-})
-let result3 = uaM.generateUa(60, {
-    device: ['pc', '80^mobile'],
-    pcOs: ['macos'],
-    mobileOs: ['80^android', 'ios'],
-})
 
 console.log('\n~~~~~~~~~~~~~~ origin ~~~~~~~~~~~~~~~~')
-console.log(result1.join('\n'))
+console.log(uaT.generateUa(50).join('\n'))
 
-console.log('\n~~~~~~~~~~~~~~ trans ~~~~~~~~~~~~~~~~')
-console.log(result2.join('\n'))
+console.log('\n~~~~~~~~~~~~~~ pc windows qb ~~~~~~~~~~~~~~~~')
+console.log(uaT.generateUa(5,{
+    device: ['pc'],
+    pcOs: ['windows'],
+    windowsApp: ['qb'],
+}).join('\n'))
 
-console.log('\n~~~~~~~~~~~~~~ min ~~~~~~~~~~~~~~~~')
-console.log(result3.join('\n'))
+console.log('\n~~~~~~~~~~~~~~ pc macos firefox ~~~~~~~~~~~~~~~~')
+console.log(uaT.generateUa(5,{
+    device: ['pc'],
+    pcOs: ['macos'],
+    macosApp: ['firefox'],
+}).join('\n'))
+
+console.log('\n~~~~~~~~~~~~~~ mobile ios app ~~~~~~~~~~~~~~~~')
+console.log(uaT.generateUa(5,{
+    device: ['mobile'],
+    mobileOs: ['ios'],
+    iosApp: ['wechat', 'qb'],
+}).join('\n'))
+
+console.log('\n~~~~~~~~~~~~~~ mobile android brand ~~~~~~~~~~~~~~~~')
+console.log(uaT.generateUa(5,{
+    device: ['mobile'],
+    mobileOs: ['android'],
+    androidBrand: ['huawei', 'samsung'],
+}).join('\n'))
+
+console.log('\n~~~~~~~~~~~~~~ mobile android app ~~~~~~~~~~~~~~~~')
+console.log(uaT.generateUa(5,{
+    device: ['mobile'],
+    mobileOs: ['android'],
+    androidApp: ['baidu', 'native'],
+}).join('\n'))
